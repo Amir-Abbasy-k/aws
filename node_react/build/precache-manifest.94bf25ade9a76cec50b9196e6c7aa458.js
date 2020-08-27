7150639943e4c0883dd981dedb74956d644bcebc
Amir-Abbasy-k <amirabbasyk@gmail.com>
2020-08-27 14:41:04 +0530

    build

---
+++ node_react/build/precache-manifest.94bf25ade9a76cec50b9196e6c7aa458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695dc82998b672bb2c751ed742dfd629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f7e5f7e530f69e9602",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d634b92862562113eb0e",
+    "url": "/static/js/2.fe640a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe640a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f7e5f7e530f69e9602",
+    "url": "/static/js/main.9fa60f3f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
